50a134a16eddc130e43159ddafd5b31222551735
skmz99 <83142392+skmz99@users.noreply.github.com>
2022-12-30 22:54:45 -0600

    Now shows information from Table

--- application/src/pages/Home.js
+++ application/src/pages/Home.js
@@ -1,24 +1,26 @@
-import React, { useState, useEffect } from "react";
-import '../css/home.css'
+import React, {useState, useEffect} from "react";
+import {Html5Table as WindowTable} from "window-table";
+import {columns} from "../content/Columns";
+import "../css/home.css"
 
 function Home(){
-    const [championName, setchampionName] = useState();
-    const [displayChamp, setdisplayChamp] = useState('LOL_Icon');
+    const [championName,setchampionName] = useState();
+    const [displayChamp,setdisplayChamp] = useState('LOL_Icon');
     const [dataSet, setdataSet] = useState();
     const [isBusy,setBusy] = useState(true);
 
-    useEffect(() =>{
-        setBusy(true);
+    useEffect(()=>{
+        setBusy(true)
         fetch('http://localhost:5000/championName')
-        .then((res) => {return res.json();})
-        .then((value) => {setchampionName(value.result);})
-        setBusy(false);
+        .then((res)=>{return res.json();})
+        .then((value)=>{setchampionName(value.result);})
+        setBusy(false)
     },[])
 
-    useEffect(() =>{
+    useEffect(()=>{
         fetch('http://localhost:5000/allVar')
-        .then((res) => {return res.json();})
-        .then((value) => {setdataSet(value.result)})
+        .then((res)=>{return res.json();})
+        .then((value)=>{setdataSet(value.result)})
     },[])
 
     if(!isBusy){
@@ -28,28 +30,33 @@ function Home(){
                 <div className={"rounded_container"}>
                     <div className={"dropdown"}>
                         <img className={"top_left"} src={require(`../imgs/${displayChamp}.jpg`)} alt = "Default League of Legends Icon"/>
-                        <div className={"dropdown_content"}>        
-                            {championName && championName.map((value,key) =>{
+                        <div className={"dropdown_content"}>
+                            {championName && championName.map((value,key)=>{
                                 return(
                                     <button key={key} onClick={()=>setdisplayChamp(value.championName)}><img id="img" src={require(`../imgs/${value.championName}.jpg`)} alt = "CHAMPION NAME"/>{value.championName}</button>
-                                    )
-                                })}
+                                )
+                            })}
                         </div>
                     </div>
-                    <div className={"center"}>
-                        {dataSet === undefined ? (
-                            'Loading...'
-                            ):(
-                                <React.Fragment> {/* React.Fragement allows for more than one component */}
-                                This project will focus on a League of Legends csv file and will output graphs/charts and other statistics
-                                {/* {console.log(dataSet) } */}
-                                </React.Fragment>
-                            )
-                        }
+                    <div className={"table_container"}>
+                            {dataSet === undefined ? (
+                                <>
+                                <div className={"center"}>Loading...</div>
+                                </>
+                                ):(
+                                    <>
+                                        <WindowTable
+                                            data={dataSet}
+                                            columns={columns}
+                                            style={{background: "#FFF"}}
+                                            className="table-sm table-bordered"
+                                        />
+                                    </>
+                            )}
                     </div>
                 </div>
             </div>
-        );
+        )
     }
 }
 
