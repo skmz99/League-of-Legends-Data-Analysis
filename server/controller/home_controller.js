e27dcb243c756f4145beac63349e7de950c23e0e
skmz99 <83142392+skmz99@users.noreply.github.com>
2023-01-20 21:48:22 -0600

    added child_process to execute python script

--- server/controller/home_controller.js
+++ server/controller/home_controller.js
@@ -1,4 +1,6 @@
 const data_table = require('../leagueCommand')
+const {spawn} = require('child_process')
+const fs = require('fs');
 const command = new data_table();
 
 const distinctChampionName = (req,res) =>{
@@ -19,5 +21,46 @@ const viewAll = (req,res,next) =>{
         res.json({result})
     })
 }
+   
+const champData = (req,res,next)=>{
+    command.champData(req.body.champName)
+    .then((result)=>{
+        res.json({result})
+    })
+}
 
-module.exports = {distinctChampionName,view,viewAll};
+const champGraph = (req,res,next)=>{
+    command.champGraph(req.body.champName)
+    .then((result)=>{
+        res.json({result})
+    }) 
+
+}
+const randomForest = (req,res,next)=>{
+    let data1;
+    if(req.body.champData !== undefined){
+        fs.writeFile('./scripts/randomForestData.txt',JSON.stringify(req.body.champData), err=>{
+            if(err){
+                console.error(err);
+            }
+        })
+    }
+    
+    const python = spawn('python', ['./scripts/randomForest.py',req.body.displayChamp] ,{stdio: 'pipe'});
+    
+    python.stdout.on('data', function(data){
+        data1 = data;           
+    })
+
+    python.stderr.on('data', (data)=>{
+        console.error(`stderr: ${data}`);
+    })
+
+    
+
+    python.on('close',(code)=>{
+        data1 = data1.toString();
+        res.json(data1);
+    })
+}
+module.exports = {distinctChampionName,view,viewAll,champData, champGraph, randomForest};
